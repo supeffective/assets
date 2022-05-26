aa581e83235aed574a97b40ce0547fa20dfad6c9
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2022-05-26 17:57:10 +0200

    generate initial set of external pokemon ref ids

--- scripts/_migrate.php
+++ scripts/_migrate.php
@@ -13,17 +13,96 @@ require_once __DIR__ . '/_bootstrap.php';
     error_reporting(-1);
     $dataSet = sgg_get_merged_pkm_entries();
 
+    // template to add new fields in particular positions (or sort existing), to the pokemon entries
+    $dataTemplate = <<<JSON
+{
+    "id": null,
+    "nid": null,
+    "dexNum": null,
+    "formId": null,
+    "name": null,
+    "formName": null,
+    "region": null,
+    "generation": null,
+    "type1": null,
+    "type2": null,
+    "color": null,
+    "abilities": {
+        "primary": null,
+        "secondary": null,
+        "hidden": null
+    },
+    "isLegendary": null,
+    "isMythical": null,
+    "isDefault": null,
+    "isForm": null,
+    "isSpecialAbilityForm": null,
+    "isCosmeticForm": null,
+    "isFemale": null,
+    "hasGenderDifferences": null,
+    "isBattleOnlyForm": null,
+    "isSwitchableForm": null,
+    "isFusion": null,
+    "fusedWith": null,
+    "isMega": null,
+    "isPrimal": null,
+    "isRegional": null,
+    "isGmax": null,
+    "canGmax": null,
+    "canDynamax": null,
+    "canBeAlpha": null,
+    "obtainableIn": [],
+    "transferableTo": [],
+    "shinyReleased": null,
+    "baseStats": {
+        "hp": null,
+        "atk": null,
+        "def": null,
+        "spa": null,
+        "spd": null,
+        "spe": null
+    },
+    "goStats": {
+        "maxCP": null,
+        "attack": null,
+        "defense": null,
+        "stamina": null
+    },
+    "weight": {
+        "avg": null,
+        "min": null,
+        "max": null,
+        "alpha": null
+    },
+    "height": {
+        "avg": null,
+        "min": null,
+        "max": null,
+        "alpha": null
+    },
+    "maleRate": null,
+    "femaleRate": null,
+    "refs": [],
+    "baseSpecies": null,
+    "baseForms": [],
+    "forms": null,
+    "evolutions": []
+}
+JSON;
+
+    $dataTemplateArr = sgg_json_decode($dataTemplate);
+
     foreach ($dataSet as $i => $pkm) {
         $pkmId = $pkm['id'];
 
-        $outputFile = sgg_get_data_path(
         // TODO rename entries-tmp to entries if changes are ok
-            'pokemon/entries-tmp/' . $pkmId . '.json'
-        );
+        $outputFile = 'pokemon/entries/' . $pkmId . '.json';
 
-        $newPkm = $pkm;
         /* --- START $newPkm data transformation */
-        // TODO: transform data here
+
+        // TODO modify $newPkm here, e.g.:
+        $newPkm = array_merge($dataTemplateArr, $pkm);
+
         /* --- END $newPkm data transformation */
         // save pkm entry
         sgg_data_save($outputFile, $newPkm, false);
