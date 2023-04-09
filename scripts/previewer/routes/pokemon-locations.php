38a169a54308b63ebc025cdaea91e41d1170f99d
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-04-09 13:52:44 +0200

    improve ui for location editor

--- scripts/previewer/routes/pokemon-locations.php
+++ scripts/previewer/routes/pokemon-locations.php
@@ -88,155 +88,180 @@ if (isset($_POST['data']) && !empty($_POST['data'])) {
         </div>
     </div>
     <div class="row">
-        <div class="col-6 text-center">
-            <form id="pkm_form" method="post" onsubmit="saveForm()" class="w-800">
+        <div class="col-12 text-center">
+            <form id="pkm_form" method="post" onsubmit="saveForm()" class="w-1200">
                 <h4>
                     <?= $pkm['name'] ?>
                 </h4>
                 <div class="icons">
-                    <i class="pkmi pkmi-<?php
+                    <i class="pkm pkm-<?php
                     echo $pkm['id']; ?>"
                     ></i>
                 </div>
-                <hr />
+                <hr/>
                 <div>
                     <button class="btn btn-lg btn-primary" type="submit">Save</button>
+                    <br/>
+                    <br/>
+                    <a
+                        class="btn btn-sm btn-link-primary"
+                        href="<?= tpl_build_url('sources/serebii', ['pid' => $pkm['id']]) ?>"
+                    >
+                        View in Serebii
+                    </a>
                 </div>
-                <br />
+                <br/>
                 <input type="hidden" id="pid" name="pid" value="<?= $pokemonId ?>">
 
-                <hr />
+                <hr/>
 
-                <h5>Debut In</h5>
-                <p>Where it was first introduced.</p>
-                <div class="gameset-input-grid">
-                    <?php
-                    foreach ($gameSets as $gameSet) {
-                        $checked = $gameSet['id'] === $pkm['debutIn'] ? ' checked ' : '';
-                        ?>
-                        <label class="gameset-tags">
+
+                <div class="row">
+                    <div class="col-6">
+                        <div class="blue-box">
+                            <h5>Obtainable In:</h5>
+                            <p style="height: 50px">
+                                Where it is obtainable in-game (this one or its the pre-evolution),
+                                without trades or special events.
+                            </p>
+                            <div class="gameset-input-grid">
+                                <?php
+                                foreach ($gameSets as $gameSet) {
+                                    $checked = in_array($gameSet['id'], $pkm['obtainableIn'], true) ? ' checked ' : '';
+                                    ?>
+                                    <label class="gameset-tags">
             <span class="gameset-tag gameset-<?= $gameSet['id'] ?>" title="<?= $gameSet['name'] ?>"
             ><?= $gameSet['id'] ?></span>
-                            <input <?= $checked ?> name="data[debutIn]" type="radio" value="<?= $gameSet['id'] ?>" />
-                        </label>
-                        <?php
-                    }
-                    ?>
-                </div>
-
-                <hr />
-
-                <h5>Obtainable In:</h5>
-                <p>Where it is obtainable in-game (this one or its the pre-evolution), without trades or special
-                    events.</p>
-                <div class="gameset-input-grid">
-                    <?php
-                    foreach ($gameSets as $gameSet) {
-                        $checked = in_array($gameSet['id'], $pkm['obtainableIn'], true) ? ' checked ' : '';
-                        ?>
-                        <label class="gameset-tags">
+                                        <input <?= $checked ?>
+                                            name="data[obtainableIn][]"
+                                            type="checkbox"
+                                            value="<?= $gameSet['id'] ?>"
+                                        />
+                                    </label>
+                                    <?php
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="blue-box">
+                            <h5>Transferrable to / Storable In:</h5>
+                            <p>Where it can be put in the box system, without losing this form.</p>
+                            <div class="gameset-input-grid">
+                                <?php
+                                foreach ($gameSets as $gameSet) {
+                                    $checked = in_array($gameSet['id'], $pkm['storableIn'], true) ? ' checked ' : '';
+                                    ?>
+                                    <label class="gameset-tags">
             <span class="gameset-tag gameset-<?= $gameSet['id'] ?>" title="<?= $gameSet['name'] ?>"
             ><?= $gameSet['id'] ?></span>
-                            <input <?= $checked ?>
-                                name="data[obtainableIn][]"
-                                type="checkbox"
-                                value="<?= $gameSet['id'] ?>"
-                            />
-                        </label>
-                        <?php
-                    }
-                    ?>
+                                        <input <?= $checked ?>
+                                            name="data[storableIn][]"
+                                            type="checkbox"
+                                            value="<?= $gameSet['id'] ?>"
+                                        />
+                                    </label>
+                                    <?php
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <hr />
+                <hr/>
 
-                <h5>Transferrable to / Storable In:</h5>
-                <p>Where it can be put in the box system, without losing this form.</p>
-                <div class="gameset-input-grid">
-                    <?php
-                    foreach ($gameSets as $gameSet) {
-                        $checked = in_array($gameSet['id'], $pkm['storableIn'], true) ? ' checked ' : '';
-                        ?>
-                        <label class="gameset-tags">
-            <span class="gameset-tag gameset-<?= $gameSet['id'] ?>" title="<?= $gameSet['name'] ?>"
-            ><?= $gameSet['id'] ?></span>
-                            <input <?= $checked ?>
-                                name="data[storableIn][]"
-                                type="checkbox"
-                                value="<?= $gameSet['id'] ?>"
-                            />
-                        </label>
+                <div class="blue-box" style="background-color: rgba(139,204,148,0.59)">
+                    <h5>Debut In</h5>
+                    <p style="height: 50px">Where it was first introduced.</p>
+                    <div class="gameset-input-grid">
                         <?php
-                    }
-                    ?>
-                </div>
-
-                <hr />
-
-                <h5>Version Exclusive In:</h5>
-                <p>
-                    More granular version of "Obtainable In". It says if it's exclusive to a particular game version.
-                    If it's in both, leave it empty.
-                </p>
-                <div class="gameset-input-grid">
-                    <?php
-                    foreach ($gameSets as $gameSet) {
-                        foreach ($gameSet['games'] as $gameId => $gameName) {
-                            $gameIdParts = explode('-', $gameId);
-                            $gameIdGame = $gameIdParts[count($gameIdParts) - 1];
-                            $checked = in_array($gameId, $pkm['versionExclusiveIn'], true) ? ' checked ' : '';
+                        foreach ($gameSets as $gameSet) {
+                            $checked = $gameSet['id'] === $pkm['debutIn'] ? ' checked ' : '';
                             ?>
                             <label class="gameset-tags">
-            <span class="gameset-tag gameset-<?= $gameId ?>" title="<?= $gameSet['name'] ?>"
-            ><?= $gameIdGame ?></span>
-                                <input <?= $checked ?>
-                                    name="data[versionExclusiveIn][]"
-                                    type="checkbox"
-                                    value="<?= $gameId ?>"
+            <span class="gameset-tag gameset-<?= $gameSet['id'] ?>" title="<?= $gameSet['name'] ?>"
+            ><?= $gameSet['id'] ?></span>
+                                <input <?= $checked ?> name="data[debutIn]"
+                                                       type="radio"
+                                                       value="<?= $gameSet['id'] ?>"
                                 />
                             </label>
                             <?php
                         }
-                    }
-                    ?>
+                        ?>
+                    </div>
                 </div>
 
-                <hr />
+                <hr/>
 
-                <h5>Event-Only In:</h5>
-                <p>Where it can be obtained, but only via Mystery Gift or special events.</p>
-                <div class="gameset-input-grid">
-                    <?php
-                    foreach ($gameSets as $gameSet) {
-                        $checked = in_array($gameSet['id'], $pkm['eventOnlyIn'], true) ? ' checked ' : '';
-                        ?>
-                        <label class="gameset-tags">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="orange-box">
+                            <h5>Version Exclusive In:</h5>
+                            <p>
+                                More granular version of "Obtainable In". It says if it's exclusive to a particular
+                                game version.
+                                If it's in both, leave it empty.
+                            </p>
+                            <div class="gameset-input-grid">
+                                <?php
+                                foreach ($gameSets as $gameSet) {
+                                    foreach ($gameSet['games'] as $gameId => $gameName) {
+                                        $gameIdParts = explode('-', $gameId);
+                                        $gameIdGame = $gameIdParts[count($gameIdParts) - 1];
+                                        $checked = in_array($gameId, $pkm['versionExclusiveIn'], true) ? ' checked ' : '';
+                                        ?>
+                                        <label class="gameset-tags">
+            <span class="gameset-tag gameset-<?= $gameId ?>" title="<?= $gameSet['name'] ?>"
+            ><?= $gameIdGame ?></span>
+                                            <input <?= $checked ?>
+                                                name="data[versionExclusiveIn][]"
+                                                type="checkbox"
+                                                value="<?= $gameId ?>"
+                                            />
+                                        </label>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="orange-box">
+
+                            <h5>Event-Only In:</h5>
+                            <p>Where it can be obtained, but only via Mystery Gift or special events.</p>
+                            <div class="gameset-input-grid">
+                                <?php
+                                foreach ($gameSets as $gameSet) {
+                                    $checked = in_array($gameSet['id'], $pkm['eventOnlyIn'], true) ? ' checked ' : '';
+                                    ?>
+                                    <label class="gameset-tags">
             <span class="gameset-tag gameset-<?= $gameSet['id'] ?>" title="<?= $gameSet['name'] ?>"
             ><?= $gameSet['id'] ?></span>
-                            <input <?= $checked ?>
-                                name="data[eventOnlyIn][]"
-                                type="checkbox"
-                                value="<?= $gameSet['id'] ?>"
-                            />
-                        </label>
-                        <?php
-                    }
-                    ?>
+                                        <input <?= $checked ?>
+                                            name="data[eventOnlyIn][]"
+                                            type="checkbox"
+                                            value="<?= $gameSet['id'] ?>"
+                                        />
+                                    </label>
+                                    <?php
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <hr />
+
+                <hr/>
                 <div>
                     <button class="btn btn-lg btn-primary" type="submit">Save</button>
                 </div>
             </form>
-        </div>
-
-        <div class="col-6 text-center">
-            <iframe
-                style="width:100%; height:1500px; border:none;"
-                src="<?= tpl_build_url('sources/serebii', ['pid' => $pkm['id']]) ?>"
-                referrerpolicy="no-referrer"
-                sandbox=""
-            ></iframe>
         </div>
     </div>
     <div class="row">
