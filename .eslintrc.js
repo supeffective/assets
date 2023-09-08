5a3e3dd894858036ce6f812129212c2d801fb6b2
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-09-08 14:31:37 +0200

    chore: upgrade eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,24 @@
+const path = require('node:path')
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    amd: true,
-    commonjs: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  ignorePatterns: [
-    '**/node_modules/**',
-    '**/.next/**',
-    '**/out/**',
-    '**/dist/**',
-    '**/generated/**',
-    'packages/website**',
-  ],
-  plugins: ['import', 'react', 'react-hooks', 'jsx-a11y', '@typescript-eslint', 'prettier'],
+  extends: [require.resolve('@r1stack/coding-style/eslint')],
   rules: {
-    'jsx-a11y/alt-text': 'error',
-    // components
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'jsx-a11y/no-noninteractive-tabindex': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/ban-types': 'warn',
-    '@typescript-eslint/ban-ts-comment': 'warn',
-    'prefer-const': 'warn',
-    'react-hooks/exhaustive-deps': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react/jsx-key': 'error',
-    'react/no-unescaped-entities': 'off',
-    'newline-before-return': 'error',
-    'react/react-in-jsx-scope': 'off',
   },
+  ignorePatterns: ['**/assets/**', '**/dist/**', '**/node_modules/**', '**/styled-system/**'],
   overrides: [
     {
-      files: ['*.js', '*.cjs'],
-      rules: {
-        'import/no-extraneous-dependencies': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-      env: {
-        node: true,
-        commonjs: true,
+      files: ['*.ts', '*.tsx', '*.mts'],
+      parserOptions: {
+        project: [path.resolve(__dirname, './packages/*/tsconfig.json')],
       },
     },
   ],
