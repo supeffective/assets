dee41405db1df769a7c564d17b1507e7deebaada
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-08-13 01:30:59 +0200

    feat(characters): import new ones and adapt script. Improve index.html

--- assets/images/characters/README.md
+++ assets/images/characters/README.md
@@ -1,10 +1,13 @@
 ## How to update
 
 - Remove the `masters-icons` folder
-- Download the "Master Characters" folder from Lewtwo's Pokémon Asset Archive.
+- Download the "Master Characters" folder from [Lewtwo's Pokémon Asset Archive](https://drive.google.com/drive/folders/11fElcd8kITai4nEGcJCgJIViGiI20X0M)
+- There are more than 4000 images (backgrounds, dream team, emotes, frames, lodge, mindscapes, scouts, icons, etc), you have to run a Finder search to find the icons and put them in a separate folder.
 - Create a new folder called `masters-icons` and place the PNGs from the "Master Characters" folder,
-ending with " Icon.png", except those that have "Unused" or "HM Icon" in their name.
-- Run `node utils/update-trainer-sprites.cjs` to rename them and generate the JSON data file.
+ending with " Icon.png" or " Icon Masters.png", except those that have "Unused", "beta" or "HM Icon" in their name.
+- Run `bun run scripts/update-trainer-sprites.cjs` to rename them (using slugized names) and generate the JSON data file.
+- Optimize the images with ImageOptim, and then commit the changes.
+
 
 
 ## Credits
--- scripts/update-trainer-sprites.cjs
+++ scripts/update-trainer-sprites.cjs
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 const dataDir = path.join(__dirname, '..', 'assets', 'images', 'characters')
 const imagesDir = path.join(__dirname, '..', 'assets', 'images', 'characters', 'masters-icons')
-const jsonFile = path.join(dataDir, 'masters-icons.json')
+const jsonFile = path.join(dataDir, 'characters.json')
 
 const characterFiles = fs.readdirSync(imagesDir).filter((file) => file.endsWith('.png'))
 const characterNames = characterFiles.map((file) => file.replace('.png', ''))
@@ -12,25 +12,47 @@ const characterNames = characterFiles.map((file) => file.replace('.png', ''))
 const characters = characterNames.map((name) => {
   const nameSlug = name
     .toLowerCase()
-    // replace whitespace with -:
+    // replace _ or whitespace with -:
     .replace(/[_\s]/g, '-')
+    .replace(/masters$/g, '')
     .replace(/icon$/g, '')
+    .replace(/ icon$/g, '')
     .replace(/-{1,}$/g, '')
     .replace(/^-{1,}$/g, '')
-    .replace(/[^a-z-0-9]/g, '')
+    .replace(/[^a-z0-9-]/g, '')
+    .replace(/-s-/g, 's-')
+    .replace(/-icon$/g, '')
+    .trim()
+
+  const cleanTitle = name
+    // replace whitespace with -
+    .replace(/_s/g, "'s")
+    .replace(/[-_+]$/g, '')
+    .replace(/ Masters$/g, '')
+    .replace(/ Icon$/g, '')
+    .trim()
 
   // Rename original file:
   const destFile = path.join(imagesDir, `${nameSlug}.png`)
   if (fs.existsSync(destFile)) {
-    throw new Error(`File already exists: ${destFile}`)
+    // throw new Error(`File already exists: ${destFile}`)
   }
 
   fs.renameSync(path.join(imagesDir, `${name}.png`), path.join(imagesDir, `${nameSlug}.png`))
 
   return {
     id: nameSlug,
-    name: name.replace(/ Icon/gi, ''),
+    name: cleanTitle,
   }
 })
 
+characters.sort((a, b) => a.name.localeCompare(b.name))
+
+const largestSlug = characters.reduce((acc, character) => {
+  return character.id.length > acc.length ? character.id : acc
+}, '')
+
 fs.writeFileSync(jsonFile, JSON.stringify(characters, null, 2))
+
+console.log(`Updated ${jsonFile}`)
+console.log(`Largest slug: ${largestSlug} (${largestSlug.length} characters)`)
